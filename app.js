79e6b904769d7bdcad026305b233002b700c90da
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-04-07 19:45:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// Question#1&2
+var myName= "Shazia"
+alert(myName);
+
+//Question#3
+var titledMsg="Hello World"
+alert(titledMsg);
+
+
+// Question#4
+var myName="shazia"
+var myAge="21 years"
+var myQualification="Graduated"
+
+alert(myName);
+alert(myAge);
+alert(myQualification);
+
+// Question#5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Question#6
+var myEmail="My email address is shazianoor943@gmail.com"
+alert(myEmail);
+
+// Question#7
+var myBook="I am trying to learn from the Book A smarter way to learn Javascript"
+alert(myBook);
+
+// Question#9
+var pattern="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(pattern);
+
+
+
